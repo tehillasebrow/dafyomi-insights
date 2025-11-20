51abef52b8505a9cae8387fa83ddde4e995a9e8e
tehillasebrow <tsebrow@student.touro.edu>
2025-11-19 21:59:15 -0500

    Reset password to 1234

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const password=process.env.NEXT_PUBLIC_ADMIN_PASSWORD
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey,password)
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
